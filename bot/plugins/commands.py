e1cfb09483789802a35e4ab41d2ba6999be0404b
joker00745 <85333471+joker00745@users.noreply.github.com>
2021-06-04 13:07:39 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -38,7 +38,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    '🏅 Our Group 🏅', url="https://t.me/AMIGOMOVIES2"
                                 )
                         ]
                     ]
